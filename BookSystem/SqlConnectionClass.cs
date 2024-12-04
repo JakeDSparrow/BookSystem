dbc011e98320c3eafa05118694085d02ebd86183
JakeDSparrow <commanderzale08@gmail.com>
2024-12-03 20:31:33 -0800

    Added a class to call the SqlConnection

---
+++ BookSystem/SqlConnectionClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSystem
+{
+    public class SqlConnectionClass
+    {
+        private string connection = "Data Source=DESKTOP-BMGGFVF;Initial Catalog=librarymini;Integrated Security=True;Trust Server Certificate=True";
+
+        public string GetConnectionString()
+        {
+            return connection;
+        }
+
+        // Method to create a SqlConnection object
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connection);
+        }
+    }
+}
