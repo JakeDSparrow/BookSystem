85c8b7ddb427bbcd31644a6a63a672aa4f6fa5eb
ADMIN <ADMIN@DESKTOP-BMGGFVF>
2024-12-03 19:11:55 -0800

    Add project files.

---
+++ BookSystem/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookSystem
+{
+    public partial class frmLogin : Form
+    {
+
+        public string username, password;
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            username = txtUsername.Text;
+            password = txtPassword.Text;
+
+
+            //dagdagan na lang to sa database, eseentially yung malalagay sa if statement here is yung select query for Admin table
+            if (username == "admin")
+            {
+                frmAdmin frmAdmin = new frmAdmin();
+                frmAdmin.ShowDialog();
+                //this.Close();
+            }
+            else 
+            {
+                frmUser frmUser = new frmUser();
+                frmUser.ShowDialog();
+                //this.Close();
+            }
+        }
+    }
+}
---
+++ BookSystem/frmAdmin.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace BookSystem
+{
+    partial class frmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblRefresh = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(681, 267);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblRefresh
+            // 
+            this.lblRefresh.Location = new System.Drawing.Point(642, 81);
+            this.lblRefresh.Name = "lblRefresh";
+            this.lblRefresh.Size = new System.Drawing.Size(100, 23);
+            this.lblRefresh.TabIndex = 1;
+            this.lblRefresh.Text = "label1";
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 386);
+            this.Controls.Add(this.lblRefresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmAdmin";
+            this.Text = "frmAdmin";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblRefresh;
+    }
+}
