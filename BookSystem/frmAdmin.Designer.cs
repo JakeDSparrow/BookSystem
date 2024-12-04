92d20fd7329c1820ed60b99e161bf45ce8cc14de
JakeDSparrow <commanderzale08@gmail.com>
2024-12-03 19:33:51 -0800

    Aded functions for frmAdmin

--- BookSystem/frmAdmin.Designer.cs
+++ BookSystem/frmAdmin.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.lblRefresh = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblAddbook = new System.Windows.Forms.Label();
+            this.lblRemoveBook = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,27 +53,58 @@
             this.lblRefresh.TabIndex = 1;
             this.lblRefresh.Text = "label1";
             // 
-            // button1
+            // lblAddbook
             // 
-            this.button1.Location = new System.Drawing.Point(13, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lblAddbook.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAddbook.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lblAddbook.Location = new System.Drawing.Point(12, 69);
+            this.lblAddbook.Name = "lblAddbook";
+            this.lblAddbook.Size = new System.Drawing.Size(100, 23);
+            this.lblAddbook.TabIndex = 2;
+            this.lblAddbook.Text = "Add Book";
+            this.lblAddbook.Click += new System.EventHandler(this.lblAddbook_Click);
+            // 
+            // lblRemoveBook
+            // 
+            this.lblRemoveBook.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRemoveBook.Location = new System.Drawing.Point(118, 69);
+            this.lblRemoveBook.Name = "lblRemoveBook";
+            this.lblRemoveBook.Size = new System.Drawing.Size(100, 23);
+            this.lblRemoveBook.TabIndex = 3;
+            this.lblRemoveBook.Text = "Remove Book";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(510, 71);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // frmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(706, 386);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblRemoveBook);
+            this.Controls.Add(this.lblAddbook);
             this.Controls.Add(this.lblRefresh);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmAdmin";
             this.Text = "frmAdmin";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,6 +112,9 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label lblRefresh;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblAddbook;
+        private System.Windows.Forms.Label lblRemoveBook;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
--- BookSystem/frmAdmin.cs
+++ BookSystem/frmAdmin.cs
@@ -16,5 +16,10 @@ namespace BookSystem
         {
             InitializeComponent();
         }
+
+        private void lblAddbook_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
