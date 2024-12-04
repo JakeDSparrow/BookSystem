0529049f67a361fb6a0148163b80a4dd7aff950c
JakeDSparrow <commanderzale08@gmail.com>
2024-12-03 19:25:32 -0800

    Added a button

--- BookSystem/frmAdmin.Designer.cs
+++ BookSystem/frmAdmin.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.lblRefresh = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,11 +50,21 @@
             this.lblRefresh.TabIndex = 1;
             this.lblRefresh.Text = "label1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(706, 386);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblRefresh);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmAdmin";
@@ -67,5 +78,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label lblRefresh;
+        private System.Windows.Forms.Button button1;
     }
 }
